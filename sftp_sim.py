03907be7062f417538a79ec91b878fad61f7f3f0
sk3l <mike@skelton.onl>
2016-11-24 16:21:30 -0500

    Add command line parsing to sftp_sim.py

--- sftp_sim.py
+++ sftp_sim.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python
 
-#class sftp_load_sim:
+import sys
+import json
 
-#    def __init__(self, acctlist):
-        
+from argparse import ArgumentParser 
 
 if __name__ == "__main__":
-    print("sftp_load_sim")
+
+    ap = ArgumentParser(add_help=False)
+
+    ap.add_argument(
+        "-a", "--accounts",
+        help="FQN of account input JSON file")
+
+    ap.add_argument(
+        "-w", "--workercnt",
+        help="Number of workers in the thread pool")
+
+    ap.add_argument(
+        "-n", "--numlimit", type=int,
+        help="Run until <numlimit> transfers hav occurred.")
+
+    ap.add_argument(
+        "-t", "--timelimit", type=int,
+        help="Run until <timelimit> seconds have elapsed.")
+
+    ap.add_argument("-h", "--help", action='store_true')
+
+    args = ap.parse_args()
+
+    if vars(args)["help"] == True or len(sys.argv) < 2:
+        ap.print_help()
